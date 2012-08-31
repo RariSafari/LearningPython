de190e2f661086726956ba88cc0d4f8d98d10e28
Natalie Jagoda <njj900@gmail.com>
2012-08-30 18:21:55 -0700

    hi

---
+++ times_table.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+#heading
+
+print "   *",
+for i in range (13):
+    print '%4d' % i,
+print
+
+#end heading
+
+for i in range (13):
+    print '%4d' % i,
+    for j in range (13):
+        print '%4d' % (i * j),
+    print
+#* 0 1 2 3 4 5 6 7 8 9 10 11 12
+#0
+#1
+#2
+#3
+#4
+#5
+#6
+#7
+#8
+#9
+#10
+#11
+#12
