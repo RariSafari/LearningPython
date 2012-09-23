0d3b1bddbcdd2d4bbc30905f938194f0b2ff2178
Natalie Jagoda <njj900@gmail.com>
2012-09-23 11:20:01 -0700

    Luke, I am your father

---
+++ StarWars.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+import os
+
+for i in xrange (50):
+    print "Luke, I am your father" 
+    os.system('say "Luke, I am your father" -v Whisper')   # Run the command say in the system shell
